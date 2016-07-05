223cfab2c452472ad9fe50785d582e88cbd7fd69
NoaneDan <Noane.Dan@outlook.com>
2016-07-05 19:48:51 +0300

    6th exercise

--- ex6_concat.js
+++ ex6_concat.js
@@ -2,6 +2,8 @@
 const http = require('http'),
       concat = require('concat-stream');
 
+// concat collects all the buffers from a stream 
+// and concatenates them together
 process.stdin.pipe(concat(function (data) {
 	console.log(data.toString().split('').reverse().join(''))	
 }))
